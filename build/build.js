30effe1e9ff7bcb2df92c67612ffafd0b171e1ac
zsq <onlymisaky@gmail.com>
2018-02-24 20:55:38 +0800

    处理静态文件夹

--- build/build.js
+++ build/build.js
@@ -1,6 +1,8 @@
+const path = require('path');
 const webpack = require('webpack');
 const ora = require('ora')
 const rimraf = require('rimraf');
+const { copySync } = require('fs-extra');
 
 const webpackConfig = require('./webpack.prod');
 const paths = require('./config/paths');
@@ -8,12 +10,16 @@ const paths = require('./config/paths');
 const tips = ora('building...');
 tips.start();
 
+// 删除 dist 目录
 rimraf(paths.dist, err => {
     if (err) throw err;
+    // 复制 static 目录到 dist/static 
+    copySync(paths.static, path.join(paths.dist, 'static'));
     // webpack --config build/webpack.prod.js
     webpack(webpackConfig, (err, stats) => {
         tips.stop();
         if (err) throw err;
+        // 输出打包信息
         process.stdout.write(stats.toString({
             colors: true,
             modules: false,
@@ -22,7 +28,6 @@ rimraf(paths.dist, err => {
             chunkModules: false
         }) + '\n\n');
 
-        tips.succeed('build complete');
+        tips.succeed('build complete' + '\n');
     });
 });
-

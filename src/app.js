375ab31a09f7af3f3e70a2126f507b09ea4a174c
zsq <onlymisaky@gmail.com>
2018-02-27 17:24:56 +0800

    app.js 取消引入uirouter

--- src/app.js
+++ src/app.js
@@ -1,9 +1,8 @@
 import angular from 'angular';
-import uiRouter from '@uirouter/angularjs';
 
 import router from './router/router.module';
 
-angular.module('app', [uiRouter, router]);
+angular.module('app', [router]);
 
 angular.element(document).ready(() => {
     angular.bootstrap(document, ['app']);

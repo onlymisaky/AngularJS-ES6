921f1da12c7692543415ed1e8045955ae9683164
mikasa <mikasa@mikasadeMacBook-Pro.local>
2018-05-08 12:01:34 +0800

    修复generator脚本

--- generator/index.js
+++ generator/index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const utils = require('./utils');
 
 const root = path.resolve(__dirname, '..');
+const templateRoot = path.resolve(root, 'generator/template')
 const fileType = process.argv[2];
 let filename = process.argv[3];
 let filePath = root + '/src/';
@@ -20,13 +21,11 @@ const FileName = utils.firstUpperCase(fileName);
 
 const reg = /<%\s*([^%>]\S+)\s*%>/;
 const reg2 = /<%if\s*(.*)\s*endif%>/;
-const reg3 = /template\\*([^%>]\S+)\s*.tpl/;
 
 let templatePathList = [];
 switch (fileType) {
     case 'directive':
         templatePathList = ['directive.js.tpl'];
-        filePath += 'directives/';
         break;
 
     case 'component':
@@ -40,31 +39,28 @@ switch (fileType) {
         ];
         if (fileType === 'view') {
             templatePathList.push('router.js.tpl');
-            filePath += 'views/';
-        } else {
-            filePath += 'components/';
         }
         break;
     default:
         break;
 }
 
+filePath = path.resolve(filePath, fileType + 's');
+
 if (fileType !== 'directive') {
-    filePath += process.argv[3];
+    filePath = path.resolve(filePath, process.argv[3]);
 }
 
 let psRead = [];
 for (var i = 0; i < templatePathList.length; i++) {
-    templatePathList[i] = path.resolve(root + '/generator/template/' + templatePathList[i]);
+    templatePathList[i] = path.resolve(templateRoot, templatePathList[i]);
     psRead.push(utils.readFilePromise(templatePathList[i], 'utf-8'));
-    // console.log(`${space}Being read : ${templatePathList[i]}`);
 }
 
 let psWrite = [];
 Promise.all(psRead).then(templateList => {
     const o = { fileType, filename, fileName, FileName };
     templateList.forEach((template, index) => {
-        // console.log(`${space}Being parsed : ${templatePathList[index]}`);
         let match = null;
         while (match = reg.exec(template)) {
             template = template.replace(match[0], o[match[1]]);
@@ -81,9 +77,8 @@ Promise.all(psRead).then(templateList => {
         if (fs.existsSync(filePath) && fileType !== 'directive') {
             filePath += '-' + Date.parse(new Date());
         }
-        const ext = '.' + reg3.exec(templatePathList[index])[1];
-        const writePath = path.resolve(filePath + '/' + filename + ext);
-
+        const ext = '.' + templatePathList[index].replace('.tpl', '').replace(templateRoot, '').replace('/', '');
+        const writePath = path.resolve(filePath, filename + ext);
         psWrite.push([writePath, template, 'utf-8']);
     });
     utils.mkdir(filePath);
